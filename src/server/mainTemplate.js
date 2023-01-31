800f00ae12847c951bb286dc680011fb76cd1cff
MegaBulkOrg <sheng.bidebao@gmail.com>
2023-01-31 17:51:57 +0300

    Rename title at the head of page and rename NPM scripts

--- src/server/mainTemplate.js
+++ src/server/mainTemplate.js
@@ -5,7 +5,7 @@ export const mainTemplate = (content, token) => `
             <meta charset="UTF-8">
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Тестовое задание для Альфа Банка</title>
+            <title>React приложение со списком новых постов из Reddit</title>
             <script src="/static/client.js" type="application/javascript"></script>
             <link rel="preconnect" href="https://fonts.googleapis.com">
             <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
